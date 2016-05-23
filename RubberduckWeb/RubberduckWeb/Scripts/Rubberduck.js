35bc569985c20c54af0d9104da4b1ad65026ab46
Abraham Hosch <hosch250@live.com>
2016-05-23 18:21:33 -0500

    Get image display working.

---
+++ RubberduckWeb/RubberduckWeb/Scripts/Rubberduck.js
@@ -0,0 +1,8 @@
+﻿var $displayImage = function (path) {
+    $("div.image-div").css("display", "block");
+    $("img.image").attr("src", path);
+};
+
+var $hideImage = function () {
+    $("div.image-div").css("display", "none");
+};
