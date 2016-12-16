ac55f1224bbd4988273d85a45403872e6e90c9c4
EBrown8534 <ebrown8534@gmail.com>
2016-12-16 11:03:57 -0500

    Fixes #26

--- RubberduckWeb/RubberduckWeb/Global.asax.cs
+++ RubberduckWeb/RubberduckWeb/Global.asax.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using RubberduckWeb.Controllers;
+using System;
+using System.Web;
+using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
@@ -12,6 +15,51 @@ namespace RubberduckWeb
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            var exception = Server.GetLastError();
+
+            // Log it here
+
+            Response.Clear();
+
+            var httpException = exception as HttpException;
+
+            var routeData = new RouteData();
+            routeData.Values.Add("controller", "Error");
+
+            if (httpException == null)
+            {
+                routeData.Values.Add("action", "Generic");
+            }
+            else
+            {
+                switch (httpException.GetHttpCode())
+                {
+                    case 404:
+                        routeData.Values.Add("action", "PageNotFound");
+                        break;
+                    case 418:
+                        // I don't know of any situation where "I'm a Teapot" will actually be an error code, but we'll add it just for fun.
+                        routeData.Values.Add("action", "Code418");
+                        break;
+                    case 500:
+                        routeData.Values.Add("action", "InternalServerError");
+                        break;
+                    default:
+                        routeData.Values.Add("action", "HttpDefault");
+                        break;
+                }
+            }
+
+            routeData.Values.Add("error", exception);
+
+            Server.ClearError();
+
+            IController controller = new ErrorController();
+            controller.Execute(new RequestContext(new HttpContextWrapper(Context), routeData));
         }
     }
 }
