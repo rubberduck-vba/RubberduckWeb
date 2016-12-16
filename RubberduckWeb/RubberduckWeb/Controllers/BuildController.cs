e635bb12a15701604bf667b748356eb22cf00475
EBrown8534 <ebrown8534@gmail.com>
2016-12-16 11:19:15 -0500

    Fixes #27

---
+++ RubberduckWeb/RubberduckWeb/Controllers/BuildController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RubberduckWeb.Controllers
+{
+    public class BuildController : Controller
+    {
+        // GET: Build
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Version(string id)
+        {
+            var version = string.Empty;
+
+            if (string.Equals(id, "stable", StringComparison.OrdinalIgnoreCase))
+            {
+                version = Assembly.GetAssembly(typeof(Rubberduck.App)).GetName().Version.ToString();
+            }
+
+            // If we leave `version` without casting to an `object`, it'll try to find a view with that name.
+            return View((object)version);
+        }
+    }
+}
