e7ea28b870400c5220f09bcdd10bf1e151db4dae
EBrown8534 <ebrown8534@gmail.com>
2017-02-03 23:49:35 -0500

    Added `IndenterController`.

---
+++ RubberduckWeb/RubberduckWeb/Controllers/IndenterController.cs
@@ -0,0 +1,18 @@
+﻿using Rubberduck.SmartIndenter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RubberduckWeb.Controllers
+{
+    public class IndenterController : Controller
+    {
+        // GET: Indenter
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
