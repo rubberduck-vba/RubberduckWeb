1307e10e0d71f7a6e9d83d23c3e76a7dfa72756f
Elliott Brown <ebrown8534@gmail.com>
2017-09-21 19:31:05 -0400

    FINISHED THE INDENTER CLOSES #21

--- RubberduckWeb/RubberduckWeb/Controllers/IndenterController.cs
+++ RubberduckWeb/RubberduckWeb/Controllers/IndenterController.cs
@@ -1,9 +1,11 @@
 ﻿using Rubberduck.SmartIndenter;
+using RubberduckWeb.Mocks;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using RubberduckWeb.Models;
 
 namespace RubberduckWeb.Controllers
 {
@@ -12,7 +14,26 @@ namespace RubberduckWeb.Controllers
         // GET: Indenter
         public ActionResult Index()
         {
-            return View();
+            return View(new IndenterSettingsModel());
+        }
+
+        [HttpPost]
+        public ActionResult Index(IndenterSettingsModel model)
+        {
+            var code = model.Code;
+
+            // Mat says this is how it works, and he seems to be right.
+            var indenter = new Indenter(null, () => model);
+            var lines = code.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+            lines = indenter.Indent(lines, false).ToArray();
+            var result = string.Join(Environment.NewLine, lines);
+
+            // Required to update Code textarea
+            // Otherwise it reloads the POSTed data
+            ModelState.Clear();
+
+            model.Code = result + Environment.NewLine;
+            return View(model);
         }
     }
 }
---
+++ RubberduckWeb/RubberduckWeb/Mocks/MockIndenterSettings.cs
@@ -0,0 +1,51 @@
+﻿using Rubberduck.SmartIndenter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RubberduckWeb.Mocks
+{
+    public class MockIndenterSettings : IIndenterSettings
+    {
+        public bool AlignCommentsWithCode { get; set; } = true;
+
+        public bool AlignContinuations { get; set; } = true;
+
+        public int AlignDimColumn { get; set; } = 15;
+
+        public bool AlignDims { get; set; } = false;
+
+        public int EndOfLineCommentColumnSpaceAlignment { get; set; } = 50;
+
+        public EndOfLineCommentStyle EndOfLineCommentStyle { get; set; } = EndOfLineCommentStyle.AlignInColumn;
+
+        public bool ForceCompilerDirectivesInColumn1 { get; set; } = false;
+
+        public bool ForceDebugStatementsInColumn1 { get; set; } = false;
+
+        public bool IgnoreOperatorsInContinuations { get; set; } = true;
+
+        public bool IndentCase { get; set; } = false;
+
+        public bool IndentCompilerDirectives { get; set; } = true;
+
+        public bool IndentEntireProcedureBody { get; set; } = true;
+
+        public bool IndentEnumTypeAsProcedure { get; set; } = false;
+
+        public bool IndentFirstCommentBlock { get; set; } = true;
+
+        public bool IndentFirstDeclarationBlock { get; set; } = true;
+
+        public int IndentSpaces { get; set; } = 4;
+
+        public int LinesBetweenProcedures { get; set; } = 1;
+
+        public bool VerticallySpaceProcedures { get; set; } = true;
+
+        public bool LegacySettingsExist() => false;
+
+        public void LoadLegacyFromRegistry() { }
+    }
+}
